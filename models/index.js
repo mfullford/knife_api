eb2ff6ae8f715f9356483ebb53442d5ebae2543c
mfullford <marissafullford@gmail.com>
2017-12-04 16:47:07 -0700

    Adds mongo heroku to index

--- models/index.js
+++ models/index.js
@@ -2,6 +2,6 @@ var mongoose = require("mongoose");
 mongoose.connect( process.env.MONGODB_URI || 
                   process.env.MONGOLAB_URI || 
                   process.env.MONGOHQ_URL || 
-                  "mongodb://localhost/knife_api");
+                  "mongodb://localhost/api");
 
 module.exports.Knife = require("./knife.js");
